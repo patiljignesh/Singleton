d55722d7c656db025459e433180367816a013cf4
Jignesh Patil <patiljignesh@gmail.com>
2022-03-29 00:58:42 +0200

    Initial Commit

---
+++ Singleton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Singleton
+//
+//  Created by Jigneshkumar Patil on 2022/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
